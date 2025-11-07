bf17b208f0d8f2b18fe856bb3b08234711cc7977
Cipriano-murilo <162237699+Cipriano-murilo@users.noreply.github.com>
2025-11-06 21:54:42 -0300

    Update script.js

--- Front-end-namo/script.js
+++ Front-end-namo/script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const API_URL = 'http://localhost:3000';
+  const API_URL = 'https://sitenamo.onrender.com';
 
   const loginForm = document.getElementById('loginForm');
   if (loginForm) {
